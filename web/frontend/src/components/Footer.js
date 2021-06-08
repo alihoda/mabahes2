0788b4325a8e7b7b029d542dc4a534640b97ecaa
milar7 <miladir@yahoo.com>
2021-06-08 11:08:00 +0430

    remove footer tag

--- web/frontend/src/components/Footer.js
+++ web/frontend/src/components/Footer.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Segment } from "semantic-ui-react";
 
 function Footer() {
-    return (
-        <footer>
-            <Segment textAlign="center">Copyright &copy; MA 2021</Segment>
-        </footer>
-    );
+  return (
+    <footer>
+      <Segment textAlign="center" vertical>
+        Copyright &copy; MA 2021
+      </Segment>
+    </footer>
+  );
 }
 
 export default Footer;

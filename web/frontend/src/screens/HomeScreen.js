cfee6de4c604c1150d1ccd6c9e026e2a157168c6
milar7 <miladir@yahoo.com>
2021-06-08 11:00:40 +0430

    add padding to card segment

--- web/frontend/src/screens/HomeScreen.js
+++ web/frontend/src/screens/HomeScreen.js
@@ -3,34 +3,34 @@ import { useDispatch, useSelector } from "react-redux";
 import { Card, Header, Segment } from "semantic-ui-react";
 
 import Product from "../components/product/Product";
-import { listProducts } from "../actions/productActions";
+import { listProduct } from "../actions/productActions";
 
 function HomeScreen() {
-    const dispatch = useDispatch();
-    const productList = useSelector((state) => state.productList);
-    const { error, loading, products } = productList;
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { products } = productList;
 
-    useEffect(() => {
-        dispatch(listProducts());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(listProduct());
+  }, [dispatch]);
 
-    return (
-        <div>
-            <Segment placeholder>
-                <Header textAlign="center" as="h1">
-                    Here Is Where You Introduce Your Projects
-                </Header>
-            </Segment>
+  return (
+    <div>
+      <Segment placeholder>
+        <Header textAlign="center" as="h1">
+          Here Is Where You Introduce Your Projects
+        </Header>
+      </Segment>
 
-            <Segment vertical>
-                <Card.Group>
-                    {products.map((product) => (
-                        <Product key={product.id} product={product} />
-                    ))}
-                </Card.Group>
-            </Segment>
-        </div>
-    );
+      <Segment basic padded="very">
+        <Card.Group>
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </Card.Group>
+      </Segment>
+    </div>
+  );
 }
 
 export default HomeScreen;

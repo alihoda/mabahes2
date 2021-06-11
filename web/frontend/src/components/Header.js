15ad3b741f6a888e2574839aa1d64803caf32998
milar7 <miladir@yahoo.com>
2021-06-11 23:21:18 +0430

    edit menu buttons

--- web/frontend/src/components/Header.js
+++ web/frontend/src/components/Header.js
@@ -21,33 +21,44 @@ function Header() {
         Home
       </Menu.Item>
 
-      <Menu.Menu position="right">
-        {userInfo ? (
+      {userInfo ? (
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Button
+              content="New Product"
+              icon="plus"
+              labelPosition="left"
+              as={Link}
+              to="/new-product"
+            />
+          </Menu.Item>
+
           <Menu.Item>
             <Button.Group>
               {/* User profile button */}
-              <Button primary animated="fade" as={Link} to={`/user/${userInfo.user.id}`}>
-                <Button.Content visible>{userInfo.user.name}</Button.Content>
-                <Button.Content hidden>Profile</Button.Content>
-              </Button>
+              <Button
+                primary
+                content={userInfo.name}
+                icon="user"
+                labelPosition="left"
+                as={Link}
+                to={`/user/${userInfo.id}`}
+              />
               {/* Logout button */}
-              <Button animated="fade" color="purple" onClick={logoutHandler}>
-                <Button.Content hidden>Logout</Button.Content>
-                <Button.Content visible>
-                  <Icon name="power off" />
-                </Button.Content>
-              </Button>
+              <Button icon="power off" color="purple" onClick={logoutHandler} />
             </Button.Group>
           </Menu.Item>
-        ) : (
+        </Menu.Menu>
+      ) : (
+        <Menu.Menu position="right">
           <Menu.Item>
             <Button primary icon labelPosition="left" as={Link} to={"/login"}>
               <Icon name="user" />
               Login
             </Button>
           </Menu.Item>
-        )}
-      </Menu.Menu>
+        </Menu.Menu>
+      )}
     </Menu>
   );
 }

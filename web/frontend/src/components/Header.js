6381872082f204750dcb62dd3e87595e360957fe
milar7 <miladir@yahoo.com>
2021-06-12 11:34:46 +0430

    edit menu buttons

--- web/frontend/src/components/Header.js
+++ web/frontend/src/components/Header.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Icon, Menu } from "semantic-ui-react";
+import { Icon, Menu } from "semantic-ui-react";
 
 import { logout } from "../actions/userActions";
 
 function Header() {
+  const [activeItem, setActiveItem] = useState("HOME");
+
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
@@ -15,48 +17,57 @@ function Header() {
     dispatch(logout());
   };
 
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
   return (
-    <Menu>
-      <Menu.Item header as={Link} to="/">
-        Home
-      </Menu.Item>
+    <Menu size="large" color="teal">
+      <Menu.Item
+        icon="home"
+        name="home"
+        as={Link}
+        to="/"
+        active={activeItem === "home"}
+        onClick={handleItemClick}
+      />
+      <Menu.Item
+        icon="tags"
+        name="tags"
+        as={Link}
+        to="/tags"
+        active={activeItem === "tags"}
+        onClick={handleItemClick}
+      />
 
       {userInfo ? (
         <Menu.Menu position="right">
-          <Menu.Item>
-            <Button
-              content="New Product"
-              icon="plus"
-              labelPosition="left"
-              as={Link}
-              to="/new-product"
-            />
-          </Menu.Item>
-
-          <Menu.Item>
-            <Button.Group>
-              {/* User profile button */}
-              <Button
-                primary
-                content={userInfo.name}
-                icon="user"
-                labelPosition="left"
-                as={Link}
-                to={`/user/${userInfo.id}`}
-              />
-              {/* Logout button */}
-              <Button icon="power off" color="purple" onClick={logoutHandler} />
-            </Button.Group>
-          </Menu.Item>
+          <Menu.Item
+            icon="plus"
+            name="new product"
+            as={Link}
+            to="/new-product"
+            active={activeItem === "new product"}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            icon="user"
+            name={userInfo.name}
+            as={Link}
+            to={`/user/${userInfo.id}`}
+            active={activeItem === `${userInfo.name}`}
+            onClick={handleItemClick}
+          />
+          <Menu.Item icon="power off" name="logout" onClick={logoutHandler} />
         </Menu.Menu>
       ) : (
         <Menu.Menu position="right">
-          <Menu.Item>
-            <Button primary icon labelPosition="left" as={Link} to={"/login"}>
-              <Icon name="user" />
-              Login
-            </Button>
-          </Menu.Item>
+          <Menu.Item
+            icon="user"
+            name="login"
+            as={Link}
+            to="/login"
+            active={activeItem === "login"}
+            onClick={handleItemClick}
+          />
         </Menu.Menu>
       )}
     </Menu>
